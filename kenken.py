c13d4f00200d58f67d06a4fa8f3502b265aa0843
Abdelrhman-CaT <mindenslavement@gmail.com>
2022-05-31 19:12:38 +0200

    adding a function to select an operation

--- kenken.py
+++ kenken.py
@@ -6,3 +6,22 @@ def is_two_cells_are_adjacent(xy1, xy2):
     dy = y1 - y2
 
     return (dx == 0 and abs(dy) == 1) or (dy == 0 and abs(dx) == 1)
+
+# define all operations that can be performed on a kenken puzzle
+
+
+def select_operation(inputOperator):
+    if inputOperator == '+':
+        return lambda a, b: a + b
+
+    elif inputOperator == '-':
+        return lambda a, b: a - b
+
+    elif inputOperator == '*':
+        return lambda a, b: a * b
+
+    elif inputOperator == '/':
+        return lambda a, b: a / b
+
+    else:
+        return None
